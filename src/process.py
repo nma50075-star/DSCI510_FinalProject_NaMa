fb704a1111e93e2c7afa602ec454c8ba4cb7939e
nma50075-star <nma50075@usc.edu>
2026-04-12 03:08:45 -0700

    Update process.py

--- src/process.py
+++ src/process.py
@@ -1,4 +1,8 @@
+import pandas as pd
+
 def clean_price(df):
     df = df.copy()
-    df["price"] = df["price"].replace("[$,]", "", regex=True).astype(float)
+    df["price"] = df["price"].replace("[$,]", "", regex=True)
+    df["price"] = pd.to_numeric(df["price"], errors="coerce")
+    df = df.dropna(subset=["price"])
     return df
