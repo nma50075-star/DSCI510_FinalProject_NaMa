5dbf9d36996d6b9965002957d04c308a09709e64
nma50075-star <nma50075@usc.edu>
2026-04-22 03:59:55 -0700

    Update data_collection.py

--- src/data_collection.py
+++ src/data_collection.py
@@ -2,14 +2,12 @@ import requests
 
 def get_location_data(city):
     url = "https://nominatim.openstreetmap.org/search"
-
     params = {"q": city,"format": "json"}
     headers = {"User-Agent": "DSCI510 final project"}
     response = requests.get(url, params=params, headers=headers)
     data = response.json()
-
     return data
 
 if __name__ == "__main__":
     result = get_location_data("Los Angeles")
-    print(result)
+    print(result[:1])
