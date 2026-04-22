f952901887e0f5b8f09a2618476d9c90c0811cf0
nma50075-star <nma50075@usc.edu>
2026-04-22 03:50:06 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,2 +1,15 @@
-DATA_FILE_1 = "data/insideairbnb.csv"
+AIRBNB_KAGGLE_FILE = "data/airbnb_kaggle.csv"
+INSIDEAIRBNB_LA_FILE = "data/insideairbnb_la.csv"
+TOURISM_FILE = "data/tourism.csv"
+
 RESULTS_FOLDER = "results/"
+
+PRICE_COL = "price"
+ROOM_TYPE_COL = "room_type"
+MINIMUM_NIGHTS_COL = "minimum_nights"
+NUMBER_OF_REVIEWS_COL = "number_of_reviews"
+
+TOURISM_TIME_COL = "time"
+TOURISM_TOTAL_COL = "total"
+TOURISM_ASIA_COL = "asia"
+TOURISM_CENTRAL_AMERICA_COL = "central_america"
