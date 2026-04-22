3fea778d1b79cc9b89426b912bcfcecb0157c2a2
nma50075-star <nma50075@usc.edu>
2026-04-21 19:06:48 -0700

    Update analyze.py

--- src/analyze.py
+++ src/analyze.py
@@ -16,3 +16,28 @@ def average_price_by_room_type(df):
 
 def price_vs_accommodates(df):
     return df[["accommodates", "price"]]
+
+def plot_room_type_price(df):
+    avg_price = df.groupby("room_type")["price"].mean()
+
+    plt.figure()
+    avg_price.plot(kind="bar")
+
+    plt.title("Average Price by Room Type")
+    plt.xlabel("Room Type")
+    plt.ylabel("Average Price")
+
+    plt.savefig(RESULTS_FOLDER + "room_type_price.png")
+    plt.close()
+
+def plot_accommodates_price(df):
+    plt.figure()
+
+    plt.scatter(df["accommodates"], df["price"])
+
+    plt.title("Price vs Accommodates")
+    plt.xlabel("Accommodates")
+    plt.ylabel("Price")
+
+    plt.savefig(RESULTS_FOLDER + "accommodates_price.png")
+    plt.close()
