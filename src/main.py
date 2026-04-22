3999d73725da8d22755d7895c787d516de39df6a
nma50075-star <nma50075@usc.edu>
2026-04-21 19:08:47 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from src.load import load_insideairbnb
 from src.process import clean_price
 from src.analyze import plot_price_histogram
+from src.analyze import plot_room_type_price
+from src.analyze import plot_accommodates_price
 
 def main():
     df = load_insideairbnb()
     df = clean_price(df)
     plot_price_histogram(df)
+    plot_room_type_price(df)
+    plot_accommodates_price(df)
 
 if __name__ == "__main__":
     main()
