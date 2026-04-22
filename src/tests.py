fdc514844548fd00348dc3c9c8ea61beaa295c32
nma50075-star <nma50075@usc.edu>
2026-04-22 03:58:07 -0700

    Update tests.py

--- src/tests.py
+++ src/tests.py
@@ -1,9 +1,26 @@
-from src.data_collection import get_location_data
+import pandas as pd
+from src.process import clean_price
+from src.process import clean_tourism
 
-def test_api():
-    data = get_location_data("Los Angeles")
-    assert len(data) > 0
+def test_clean_price():
+    df = pd.DataFrame({"price": ["$100", "$2,500", "$80"]})
+    cleaned_df = clean_price(df)
+    assert cleaned_df["price"].tolist() == [100, 2500, 80]
+
+def test_clean_tourism():
+    df = pd.DataFrame({
+        "time": ["2000-01", "2000-02"],
+        "total": ["2,866,229", "2,948,121"],
+        "asia": ["532,666", "570,342"],
+        "central_america": ["64,922", "55,336"]
+})
+
+    cleaned_df = clean_tourism(df)
+    assert cleaned_df["total"].tolist() == [2866229, 2948121]
+    assert cleaned_df["asia"].tolist() == [532666, 570342]
+    assert cleaned_df["central_america"].tolist() == [64922, 55336]
 
 if __name__ == "__main__":
-    test_api()
-    print("API test passed")
+    test_clean_price()
+    test_clean_tourism()
+    print("All tests passed.")
