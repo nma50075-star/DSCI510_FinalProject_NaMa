0d4ee606c45856167d73afc10944919555c8c886
nma50075-star <nma50075@usc.edu>
2026-04-21 19:22:46 -0700

    Update load.py

--- src/load.py
+++ src/load.py
@@ -2,5 +2,7 @@ import pandas as pd
 from src.config import DATA_FILE_1
 
 def load_insideairbnb():
-    df = pd.read_csv(DATA_FILE_1)
+    df = pd.read_csv(DATA_FILE_1, low_memory=False)
+    df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+    print(df.columns)
     return df
